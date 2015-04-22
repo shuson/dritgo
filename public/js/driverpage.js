25adbfaa9ee7d36c758a0a5f9f5504e38a2d44b7
nevermoi <wlgq@hotmail.com>
2015-04-22 20:53:06 +0800

    contact popover is done

--- public/js/driverpage.js
+++ public/js/driverpage.js
@@ -2,4 +2,8 @@ $(function(){
     $(function () {
         $(".youtube").YouTubeModal({autoplay:0, width:640, height:480});
     });
+    
+    $(function () {
+      $('[data-toggle="popover"]').popover()
+    })
 });
