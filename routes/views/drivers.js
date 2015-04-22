210277c23c9d2d3550e991bc669a54b6605a638f
nevermoi <wlgq@hotmail.com>
2015-04-22 22:32:02 +0800

    add keyword search

--- routes/views/drivers.js
+++ routes/views/drivers.js
@@ -11,13 +11,16 @@ exports = module.exports = function(req, res) {
     
     locals.dateFrom = req.body.dateFrom;
     locals.dateTo = req.body.dateTo;
+    locals.keyword = req.body.keyword;
 	
 	// Load the drivers
 	view.on('init', function(next) {
-		
+
 		var q = keystone.list('Driver').model.find().where('avaliable', true);
-		
+		q.or({"description" : new RegExp(req.body.keyword, "i")}, {"name": new RegExp(req.body.keyword, "i")});
 		q.exec(function(err, results) {
+            if(results.length === 0) req.flash('warning', 'Driver not found.');
+                
 			locals.drivers = results;
 			next(err);
 		});
